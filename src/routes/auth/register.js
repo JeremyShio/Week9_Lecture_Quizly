b96d98823cb7bc5b00c95cc536a0d81b22ee9c6b
Jeremy <jeremy.shiotani@gmail.com>
2022-09-08 08:34:49 -0700

    Added launch json to quiz application

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,7 +1,15 @@
+const { User } = require('../../models')
+
+
+
+
 module.exports = (req, res) => {
     if (req.body.password !== req.body.confirmPass) {
         res.send({error: "Your passwords do NOT match"})
         return
     }
-    res.send('Post Register from register')
+    const { username, email, password } = req.body
+    const user = new User({ username, email, password })
+    user.save();
+    res.send(`New User Created - ${user.username}`)
 }
